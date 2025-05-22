c302ae3a75a2b3a90a53f6c259294588c1b25294
Mohammad Anvaari <mohamad.anvari@snapp.cab>
2025-05-22 21:30:31 +0330

    Add class to handle app configs using pydantin_settings

---
+++ app/core/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_ignore_empty= False,
+        env_file=".env",
+        env_file_encoding='utf-8'
+    )
+
+    telegram_bot_token: str = Field(..., alias="telegram_bot_tk")
+    telegram_chat_id: str = Field(..., alias="telegram_chat")
+    openai_api_key: str = Field(..., alias="openai_api_k")
+    aragorn_token: str = Field(...,alias="aragorn_tk")
+    log_level: str
+    database_file_path: str
+
+app_settings = Settings() # type: ignore
+
