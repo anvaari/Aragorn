4e6d9610414e103f8bcdc2d94afa3dac547e33f8
Mohammad Anvaari <mohamad.anvari@snapp.cab>
2025-05-23 17:00:15 +0330

    Change add_event to manual_event and add ig_event endpoint

--- app/api/v1/endpoints/events.py
+++ app/api/v1/endpoints/events.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from models.event import EventCreate
-from services.event_service import post_event_on_telegram
+from services.event_service import post_manual_event_on_telegram,post_ig_event_on_telegram
 from core.config import app_settings
 from log.logger import get_app_logger
 from fastapi import Depends, HTTPException
@@ -20,6 +20,10 @@ def verify_bearer_token(
         raise HTTPException(status_code=403, detail="Invalid token")
 
 
-@router.post("/add_event",dependencies=[Depends(verify_bearer_token)])
-def add_event(event: EventCreate):
-    return post_event_on_telegram(event)
+@router.post("/manual_event",dependencies=[Depends(verify_bearer_token)])
+def manual_event(event: EventCreate):
+    return post_manual_event_on_telegram(event)
+
+@router.post("/ig_event",dependencies=[Depends(verify_bearer_token)])
+def ig_event(ig_link: str):
+    return post_ig_event_on_telegram(ig_link)
